c1fc378a209a20509c8f1c0c7d3d0f5e05110b30
Alphonsce <varlamov.al@phystech.edu>
2022-02-02 10:23:56 +0300

    function is done

--- graphs/one33/SME.py
+++ graphs/one33/SME.py
@@ -1,17 +1,44 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from math import sqrt
 
 df = pd.DataFrame({'Apples': [35, 41], 'Bananas': [21, 34]})
 
-print(df.Apples.mean())
+print((df.Apples ** 2).mean(), len(df.Apples))
 
 plt.plot(df.Apples, [1, 2])
 
 plt.show()
 
 def calculate_k(x, y, through_zero=False):
+    '''
+    Вычисление коэффициентов для аппроксимации зависимостью y = kx + b
+    '''
+    n = len(x)
+    m_x = x.mean()
+    m_y = y.mean()
+    m_xx = (x * x). mean()
+    m_yy = (y * y).mean()
+    m_xy = (x * y).mean()
+    m_x_m_x = x.mean() * x.mean()
+    m_y_m_y = y.mean() * y.mean()
+
+    k = 0
+    s_k = 0
+    b = 0
+    s_b = 0
+
     if through_zero:
-        pass
-    
+        k = m_xy / m_xx
+        s_k = (1 / sqrt(n)) * sqrt((m_yy / m_xx) - k ** 2)
+        return [k, s_k]
+
     else:
-        pass
+        k = (m_xy - m_x * m_y) / (m_xx - m_x_m_x)
+        b = m_y - k * m_x
+
+        s_k = (1 / sqrt(n)) * sqrt((m_yy - m_y_m_y) / (m_xx - m_x_m_x) - k ** 2)
+        s_b = s_k * sqrt(m_xx - m_x_m_x)
+        return [k, s_k, b, s_b]
+
+        
